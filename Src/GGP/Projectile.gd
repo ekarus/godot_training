3c31e86ed4a6638ab48059c4328a7f4930f6719d
Ruslan <ekarus303@gmail.com>
2020-06-03 14:56:15 +0300

    lifetime for projectiles

--- Src/GGP/Projectile.gd
+++ Src/GGP/Projectile.gd
@@ -3,13 +3,18 @@ class_name Projectile
 
 export var speed := 1650.0
 export var damage := 10.0
+export var lifetime := 1.5
 
 var direction := Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	direction =  Vector2(sin(-rotation), cos(rotation))
-	pass # Replace with function body.
+	var timer = Timer.new()
+	timer.connect("timeout", self, "queue_free")
+	timer.wait_time = lifetime
+	timer.autostart = true
+	add_child(timer)
 
 func _physics_process(delta: float) -> void:
 	var collision := move_and_collide(speed * direction * delta)
